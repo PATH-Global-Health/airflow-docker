1e55f1a75c0af799256b799b5b1d2c73f0975314
Belendia Serda <belendia@gmail.com>
2023-03-14 11:22:23 +0300

    Add task group for org unit processing

--- dags/hmis_dhis2.py
+++ dags/hmis_dhis2.py
@@ -3,11 +3,10 @@ from airflow import DAG
 from airflow.providers.postgres.operators.postgres import PostgresOperator
 from airflow.operators.python import PythonOperator
 from airflow.hooks.base_hook import BaseHook
-from macepa_plugin import DHIS2MetadataDownloadOperator, GeneratePostgreSQLOperator
 
-import json
-from pandas import json_normalize
-from datetime import datetime, timedelta
+from datetime import timedelta
+
+from hmis_groups.process_org_units_metadata import process_org_units_metadata
 
 default_args = {
     'owner': 'airflow',
@@ -17,7 +16,7 @@ default_args = {
     'email_on_failure': False,
     'email_on_retry': False,
     'retries': 1,
-    'retry_delay': timedelta(minutes=5),
+    'retry_delay': timedelta(days=15),
 }
 
 # ti - task instance
@@ -31,7 +30,7 @@ def _set_data_source(ti, http_conn_id):
 
 
 with DAG('HMIS-DHIS2',  default_args=default_args,
-         description='''A pipeline for reading data from HMIS/DHIS2 and storing it in a 
+         description='''A pipeline for reading data from HMIS/DHIS2 and storing it in a
          PostgreSQL staging database, after which the data is transformed and stored in the ClickHouse.''',
          schedule='0 19 * * *', catchup=False) as dag:
 
@@ -53,47 +52,7 @@ with DAG('HMIS-DHIS2',  default_args=default_args,
         op_kwargs={"http_conn_id": 'hmis_dhis2_api'},
     )
 
-    extract_organisation_unit = DHIS2MetadataDownloadOperator(
-        task_id='extract_organisation_unit',
-        endpoint='organisationUnits',
-        http_conn_id='hmis_dhis2_api',
-        fields='*'
-    )
-
-    change_json_2_sql_org_unit = GeneratePostgreSQLOperator(
-        task_id='change_json_2_sql_org_unit',
-        table_name='organisationunit',
-        json_key_table_columns_2_map={
-            'id': {'column': 'uid', 'type': 'str'},
-            'code': {'column': 'code', 'type': 'str'},
-            'created': {'column': 'created', 'type': 'timestamp'},
-            'lastUpdated': {'column': 'lastupdated', 'type': 'timestamp'},
-            'name': {'column': 'name', 'type': 'str'},
-            'shortName': {'column': 'shortname', 'type': 'str'},
-            'parent.id': {'column': 'parentid', 'type': 'str'},
-            'path': {'column': 'path', 'type': 'str'},
-            'leaf': {'column': 'leaf', 'type': 'bool'},
-            'openingDate': {'column': 'openingdate', 'type': 'date'},
-            'closedDate': {'column': 'closeddate', 'type': 'date'},
-            'contactPerson': {'column': 'contactperson', 'type': 'str'},
-            'address': {'column': 'address', 'type': 'str'},
-            'email': {'column': 'email', 'type': 'str'},
-            'phoneNumber': {'column': 'phonenumber', 'type': 'str'},
-            'geometry.type': {'column': 'featuretype', 'type': 'str'},
-            'geometry.coordinates': {'column': 'coordinates', 'type': 'str'}
-        },
-        primary_keys=[
-            'uid', 'source_id'
-        ],
-        sql_filename="organisationUnits.sql",
-        json_file="dags/tmp/json/organisationUnits.json"
-    )
-
-    import_org_units = PostgresOperator(
-        task_id='import_org_units',
-        postgres_conn_id='postgres',
-        sql="tmp/pg_sql/organisationUnits.sql"
-    )
+    process_hmis_org_units_metadata = process_org_units_metadata()
 
     create_staging_tables >> populate_data_source_tables >> set_data_source >> \
-        extract_organisation_unit >> change_json_2_sql_org_unit >> import_org_units
+        process_hmis_org_units_metadata
---
+++ dags/hmis_groups/process_org_units_metadata.py
@@ -0,0 +1,54 @@
+
+from airflow.utils.task_group import TaskGroup
+from airflow.providers.postgres.operators.postgres import PostgresOperator
+
+from macepa_plugin import DHIS2MetadataDownloadOperator, GeneratePostgreSQLOperator
+
+
+def process_org_units_metadata():
+    with TaskGroup('process_org_units_metadata', tooltip='Download and import orgunits into the staging database') as group:
+        extract_organisation_unit = DHIS2MetadataDownloadOperator(
+            task_id='extract_organisation_unit',
+            endpoint='organisationUnits',
+            http_conn_id='hmis_dhis2_api',
+            fields='*'
+        )
+
+        change_json_2_sql_org_unit = GeneratePostgreSQLOperator(
+            task_id='change_json_2_sql_org_unit',
+            table_name='organisationunit',
+            json_key_table_columns_2_map={
+                'id': {'column': 'uid', 'type': 'str'},
+                'code': {'column': 'code', 'type': 'str'},
+                'created': {'column': 'created', 'type': 'timestamp'},
+                'lastUpdated': {'column': 'lastupdated', 'type': 'timestamp'},
+                'name': {'column': 'name', 'type': 'str'},
+                'shortName': {'column': 'shortname', 'type': 'str'},
+                'parent.id': {'column': 'parentid', 'type': 'str'},
+                'path': {'column': 'path', 'type': 'str'},
+                'leaf': {'column': 'leaf', 'type': 'bool'},
+                'openingDate': {'column': 'openingdate', 'type': 'date'},
+                'closedDate': {'column': 'closeddate', 'type': 'date'},
+                'contactPerson': {'column': 'contactperson', 'type': 'str'},
+                'address': {'column': 'address', 'type': 'str'},
+                'email': {'column': 'email', 'type': 'str'},
+                'phoneNumber': {'column': 'phonenumber', 'type': 'str'},
+                'geometry.type': {'column': 'featuretype', 'type': 'str'},
+                'geometry.coordinates': {'column': 'coordinates', 'type': 'str'}
+            },
+            primary_keys=[
+                'uid', 'source_id'
+            ],
+            sql_filename="organisationUnits.sql",
+            json_file="dags/tmp/json/organisationUnits.json"
+        )
+
+        import_org_units = PostgresOperator(
+            task_id='import_org_units',
+            postgres_conn_id='postgres',
+            sql="tmp/pg_sql/organisationUnits.sql"
+        )
+
+        extract_organisation_unit >> change_json_2_sql_org_unit >> import_org_units
+
+    return group
