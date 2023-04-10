fb88881077918c1ce998dce2ff50993e01f25763
Belendia Serda <belendia@gmail.com>
2023-04-10 14:55:44 +0300

    Add skipping data import task if no new data available

--- dags/hmis_groups/process_data.py
+++ dags/hmis_groups/process_data.py
@@ -4,7 +4,7 @@ import glob
 
 from airflow.utils.task_group import TaskGroup
 from airflow.providers.postgres.operators.postgres import PostgresOperator
-from airflow.operators.python import PythonOperator
+from airflow.operators.python import PythonOperator, BranchPythonOperator
 from airflow.hooks.postgres_hook import PostgresHook
 from airflow.models import Variable
 
@@ -20,6 +20,11 @@ def _get_hmis_last_updated(ti):
 
 def _set_hmis_last_updated():
     Variable.set("hmis_last_updated", datetime.today().strftime('%Y-%m-%d'))
+
+
+def _check_json_data_available(ti):
+    if glob.glob("dags/tmp/pg_sql/data/*.sql").__len__() > 0:
+        return 'process_data.import_data_2_staging_db'
 
 
 def query_and_push(ti, sql, postgres_conn_id):
@@ -48,7 +53,8 @@ def process_data():
 
         set_hmis_last_updated = PythonOperator(
             task_id='set_hmis_last_updated',
-            python_callable=_set_hmis_last_updated
+            python_callable=_set_hmis_last_updated,
+            trigger_rule='none_failed'
         )
 
         download_data = DHIS2DataDownloadOperator(
@@ -76,6 +82,11 @@ def process_data():
             ]
         )
 
+        is_json_data_available = BranchPythonOperator(
+            task_id='is_data_available',
+            python_callable=_check_json_data_available
+        )
+
         # PostgresOperator gets the sql files in tmp/pg_sql... and not in dags/tmp/pg_sql...
         # However glob.glob(...) gets the sql files in dags/tmp/pg_sql not in tmp/pg_sql...
         # That is why we replace dags/ with empty string to make PostgresOperator get the sql files.
@@ -84,6 +95,6 @@ def process_data():
                                                     sql=[x.replace('dags/', '') for x in glob.glob("dags/tmp/pg_sql/data/*.sql")])
 
         [get_hmis_last_updated,
-            get_org_unit_ids] >> download_data >> change_hmis_data_from_json_2_sql >> import_data_2_staging_db >> set_hmis_last_updated
+            get_org_unit_ids] >> download_data >> change_hmis_data_from_json_2_sql >> is_json_data_available >> [import_data_2_staging_db] >> set_hmis_last_updated
 
     return group
