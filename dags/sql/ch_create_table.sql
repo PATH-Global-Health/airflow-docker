7e51b7deb8c6bfcba660b5c0302c43b7108eed8a
Belendia Serda <belendia@gmail.com>
2023-04-24 11:33:42 +0300

    Change the order by clauth for orgunit table in ClickHouse

--- dags/sql/ch_create_table.sql
+++ dags/sql/ch_create_table.sql
@@ -1,2 +1,2 @@
 CREATE TABLE IF NOT EXISTS data_source ( id String, title String, url String, description String ) ENGINE = MergeTree PRIMARY KEY (id)
-CREATE TABLE IF NOT EXISTS orgunit ( uid String, name String, level UInt16, source_id String, last_updated DateTime ) ENGINE = ReplacingMergeTree(last_updated) ORDER BY (level, source_id)
+CREATE TABLE IF NOT EXISTS orgunit ( uid String, name String, level UInt16, source_id String, lastupdated DateTime ) ENGINE = ReplacingMergeTree(lastupdated) ORDER BY (uid, name, level, source_id)
