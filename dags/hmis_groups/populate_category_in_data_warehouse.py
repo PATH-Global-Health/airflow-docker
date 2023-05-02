f3742cb510cad15417527911d381bab7a8067e26
Belendia Serda <belendia@gmail.com>
2023-05-02 19:54:03 +0300

    Created tasks to import category metadata into clickhouse

--- dags/hmis_groups/populate_category_in_data_warehouse.py
+++ dags/hmis_groups/populate_category_in_data_warehouse.py
@@ -10,6 +10,8 @@ from macepa_plugin import ClickHouseMultiSqlOperator
 
 CH_CATEGORY_TABLE = 'category'
 CH_CATEGORY_TABLE_SCHEMA = 'dags/tmp/ch_sql/category_schema.sql'
+CATEGORY_METADATA_JSON_FILE = 'dags/tmp/json/category_metadata.json'
+CATEGORY_METADATA_SQL_FILE = 'dags/tmp/ch_sql/category_metadata.sql'
 
 # category_structure = {
 #         "CategoryComboID": {
@@ -39,23 +41,23 @@ CH_CATEGORY_TABLE_SCHEMA = 'dags/tmp/ch_sql/category_schema.sql'
 #     pg_df = pg_hook.get_pandas_df('''
 #         select
 #             cc.uid as CategoryComboID, cc.name as CategoryComboName,
-#             dec.uid as DataElementCategoryID, dec.name as DataElementCategoryName, 
+#             dec.uid as DataElementCategoryID, dec.name as DataElementCategoryName,
 #             dec.previous_name as DataElementCategoryPreviousName, dec.change,
 #             deco.uid as DataElementCategoryOptionID, deco.name as DataElementCategoryOptionName
-#         from dataelementcategory as dec 
-#         inner join dataelementcategory_categoryoption as decco on dec.uid=decco.category_id 
-#         inner join dataelementcategoryoption as deco on decco.categoryoption_id=deco.uid 
+#         from dataelementcategory as dec
+#         inner join dataelementcategory_categoryoption as decco on dec.uid=decco.category_id
+#         inner join dataelementcategoryoption as deco on decco.categoryoption_id=deco.uid
 #         inner join dataelementcategory_categorycombo as deccc on deccc.dataelementcategory_id=dec.uid
 #         inner join categorycombo as cc on deccc.categorycombo_id =cc.uid;
 #     ''')
 #     for index, row in pg_df.iterrows():
-#         # register the categorycombo if it is not registered 
+#         # register the categorycombo if it is not registered
 #         if row[0] not in category_structure:
 #             category_structure[row[0]] = {
 #                 "name": row[1],
 #                 "categories": {}
 #             }
-        
+
 #         if row[2] not in category_structure[row[0]]['categories']:
 #             category_structure[row[0]]['categories'][row[2]] = {
 #                 "name": row[3],
@@ -63,7 +65,7 @@ CH_CATEGORY_TABLE_SCHEMA = 'dags/tmp/ch_sql/category_schema.sql'
 #                 "change": row[5],
 #                 "categoryOptions": {}
 #             }
-        
+
 #         if row[6] not in category_structure[row[0]]['categories'][row[2]]['categoryOptions']:
 #             category_structure[row[0]]['categories'][row[2]]['categoryOptions'][row[6]] = {
 #                 "name": row[7]
@@ -72,9 +74,9 @@ CH_CATEGORY_TABLE_SCHEMA = 'dags/tmp/ch_sql/category_schema.sql'
 #     file_name = "dags/tmp/json/category_structure.json"
 #     with open(file_name, 'w') as file:
 #         file.write(json.dumps(category_structure))
-    
+
 #     ti.xcom_push(key="category_structure_file_name", value="dags/tmp/json/category_structure.json")
-    
+
 
 def generate_category_schema(ti):
     pg_hook = PostgresHook(postgres_conn_id='postgres')
@@ -83,7 +85,7 @@ def generate_category_schema(ti):
         from dataelementcategory 
         where change = 'insert' or change = 'update';
         """)
-    
+
     sql = []
     previous_field_name = ""
 
@@ -91,18 +93,90 @@ def generate_category_schema(ti):
     for index, row in pg_df.iterrows():
         if row[3] == "insert":
             if not previous_field_name:
-                sql.append('ALTER TABLE {} ADD COLUMN IF NOT EXISTS {} String AFTER lastupdated;'.format(CH_CATEGORY_TABLE, row[0]))
-                sql.append('ALTER TABLE {} ADD COLUMN IF NOT EXISTS {} String AFTER {};'.format(CH_CATEGORY_TABLE, row[1].replace(" ", ""), row[0]))
+                sql.append('ALTER TABLE {} ADD COLUMN IF NOT EXISTS {} String AFTER lastupdated;'.format(
+                    CH_CATEGORY_TABLE, row[0]))
+                sql.append('ALTER TABLE {} ADD COLUMN IF NOT EXISTS {} String AFTER {};'.format(
+                    CH_CATEGORY_TABLE, row[1].replace(" ", ""), row[0]))
             else:
-                sql.append('ALTER TABLE {} ADD COLUMN IF NOT EXISTS {} String AFTER {};'.format(CH_CATEGORY_TABLE, row[0], previous_field_name))
-                sql.append('ALTER TABLE {} ADD COLUMN IF NOT EXISTS {} String AFTER {};'.format(CH_CATEGORY_TABLE, row[1].replace(" ", ""), row[0]))
+                sql.append('ALTER TABLE {} ADD COLUMN IF NOT EXISTS {} String AFTER {};'.format(
+                    CH_CATEGORY_TABLE, row[0], previous_field_name))
+                sql.append('ALTER TABLE {} ADD COLUMN IF NOT EXISTS {} String AFTER {};'.format(
+                    CH_CATEGORY_TABLE, row[1].replace(" ", ""), row[0]))
             previous_field_name = row[1].replace(" ", "")
         elif row[3] == "update":
             # Users are only allowed to change the names of the categories not their IDs in DHIS2.
             # This is the reason to rename the columns related with name without touching the IDs.
-            sql.append('ALTER TABLE {} RENAME COLUMN IF EXISTS {} to {}'.format(CH_CATEGORY_TABLE, category['previous_name'].replace(" ", ""), category['name'].replace(" ", "")))
+            sql.append('ALTER TABLE {} RENAME COLUMN IF EXISTS {} to {}'.format(
+                CH_CATEGORY_TABLE, category['previous_name'].replace(" ", ""), category['name'].replace(" ", "")))
 
     with open(os.path.join(CH_CATEGORY_TABLE_SCHEMA), 'w') as f:
+        f.write('\n'.join(sql))
+
+
+# Data structure
+# {
+#     "categoryoptioncomboid_1": {
+#         "name": "categoryoptioncomboname",
+#         "categoryid":"categoryoption_id",
+#         "categoryname":"categoryoption_name",
+#         ...
+#     },
+#     "categoryoptioncomboid_2": {
+#         "name": "categoryoptioncomboname",
+#         "categoryid":"categoryoption_id",
+#         "categoryname":"categoryoption_name",
+#         ...
+#     }
+# }
+
+def convert_category_metadata(ti):
+    pg_hook = PostgresHook(postgres_conn_id='postgres')
+    pg_df = pg_hook.get_pandas_df("""
+       select coc.uid, coc.name,de_c.uid, de_c.name, deco.uid, deco.name, deco.source_id
+       from categoryoptioncombo as coc 
+       inner join categoryoptioncombo_categoryoptions as cocco on coc.uid = cocco.category_option_combo_id 
+       inner join dataelementcategoryoption as deco on cocco.category_option_id = deco.uid
+       inner join dataelementcategory_categoryoption as decco on decco.categoryoption_id = deco.uid 
+       inner join dataelementcategory as de_c on de_c.uid = decco.category_id
+       where deco.change = 'insert' or  deco.change = 'update'
+       order by coc.uid ;""")
+    data = {}
+
+    for index, row in pg_df.iterrows():
+        if row[0] not in data:
+            data[row[0]] = {"name": row[1], "source": row[6]}
+
+        data[row[0]][row[2]] = row[4]
+        data[row[0]][row[3]] = row[5]
+
+    with open(CATEGORY_METADATA_JSON_FILE, 'w') as file:
+        file.write(json.dumps(data))
+
+
+def convert_category_metadata_in_json_2_sql(ti):
+
+    with open(CATEGORY_METADATA_JSON_FILE, 'r', encoding="utf-8") as f:
+        categories = json.load(f)
+        sql = []
+
+        values = []
+        cols = []
+
+        for category_key, category_value in categories.items():
+            cols.append(category_key)
+            values.append(category_value[category_key]['name'])
+
+            cols.append('source_id')
+            values.append(category_value[category_key]['source'])
+
+            for category_option_key, category_option_value in categories[category_key].items():
+                cols.append(category_option_key)
+                values.append(category_option_value[category_option_key])
+
+            sql.append("INSERT INTO category ({}) VALUES ({})".format(
+                ', '.join(cols), ', '.join(values)))
+
+    with open(CATEGORY_METADATA_SQL_FILE, 'w') as f:
         f.write('\n'.join(sql))
 
 
@@ -131,7 +205,25 @@ def populate_category_in_data_warehouse():
             sql="update dataelementcategory set change = '' where change = 'update' or change = 'insert'"
         )
 
-        generate_category_columns_schema >> import_category_schema_into_ch >> reset_category_in_pgsql
+        convert_category_metadata_to_json = PythonOperator(
+            task_id='convert_category_metadata_to_json',
+            python_callable=convert_category_metadata
+        )
+
+        convert_category_metadata_in_json_to_sql = PythonOperator(
+            task_id='convert_category_metadata_in_json_to_sql',
+            python_callable=convert_category_metadata_in_json_2_sql
+        )
+
+        import_category_metadata_into_clickhouse = ClickHouseMultiSqlOperator(
+            task_id='import_category_metadata_into_clickhouse',
+            database='core',
+            clickhouse_conn_id='clickhouse',
+            sql_file=CATEGORY_METADATA_SQL_FILE
+        )
+
+        generate_category_columns_schema >> import_category_schema_into_ch >> reset_category_in_pgsql >> \
+            convert_category_metadata_to_json >> convert_category_metadata_in_json_to_sql >> \
+            import_category_metadata_into_clickhouse
 
     return group
-
