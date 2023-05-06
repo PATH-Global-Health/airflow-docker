7bea10c59aeb2deac8b283a607ab16640e533cca
Belendia Serda <belendia@gmail.com>
2023-05-06 13:40:51 +0300

    Convert data elements and groups into json

--- dags/hmis_groups/populate_dataelement_in_data_warehouse.py
+++ dags/hmis_groups/populate_dataelement_in_data_warehouse.py
@@ -14,6 +14,52 @@ DATAELEMENT_METADATA_JSON_FILE = 'dags/tmp/json/dataElementMetadata.json'
 DATAELEMENT_METADATA_SQL_FILE = 'dags/tmp/ch_sql/dataElementMetadata.sql'
 
 
+# Data structure for category metadata
+# {
+#     "dataelement_uid_1": {
+#         "name": "dataelement name",
+#         "source": "c1x82kksj"
+#         "groups": {
+#               "group_uid_1":"group name 1",
+#               "group_uid_2":"group name 2",
+#         }
+#         ...
+#     },
+#     "dataelement_uid_2": {
+#         "name": "dataelement name",
+#         "source": "c1x82kksj"
+#         "groups": {
+#               "group_uid_1":"group name 1",
+#               "group_uid_2":"group name 2",
+#         }
+#         ...
+#     }
+# }
+
+
+def convert_dataelement_metadata(ti):
+    pg_hook = PostgresHook(postgres_conn_id='postgres')
+    pg_df = pg_hook.get_pandas_df("""
+       select de.uid, de.name,deg.uid,deg.name, de.source_id from dataelement as de 
+        inner join dataelement_dataelementgroup as de_deg on de.uid = de_deg.dataelement_id 
+        inner join dataelementgroup as deg on deg.uid = de_deg.group_id;""")
+    data = {}
+
+    # row[0] - data element uid
+    # row[1] - data element name
+    # row[2] - data element group uid
+    # row[3] - data element group name
+    # row[4] - data source id
+    for index, row in pg_df.iterrows():
+        if row[0] not in data:
+            data[row[0]] = {"name": row[1], "source": row[4], "groups": {}}
+
+        data[row[0]]["groups"][row[2]] = row[3]
+
+    with open(DATAELEMENT_METADATA_JSON_FILE, 'w') as file:
+        file.write(json.dumps(data))
+
+
 def populate_dataelement_in_data_warehouse():
     with TaskGroup('populate_data_element_in_data_warehouse', tooltip='Populate the dataelement table in the data warehouse') as group:
         generate_dataelement_columns_schema = GenerateCHTableSchema(
@@ -37,6 +83,12 @@ def populate_dataelement_in_data_warehouse():
             sql="update dataelementgroup set change = '' where change = 'update' or change = 'insert'"
         )
 
-        generate_dataelement_columns_schema >> import_dataelement_schema_into_ch >> reset_dataelement_in_pgsql
+        convert_dataelement_metadata_to_json = PythonOperator(
+            task_id='convert_dataelement_metadata_to_json',
+            python_callable=convert_dataelement_metadata
+        )
+
+        generate_dataelement_columns_schema >> import_dataelement_schema_into_ch >> reset_dataelement_in_pgsql >> \
+            convert_dataelement_metadata_to_json
 
     return group
